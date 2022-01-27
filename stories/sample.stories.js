25273ce9e7a7ca5e9e1a0997fd74613fab78a616
Mueller, Edward <111723@NTTDATA.COM>
2022-01-27 13:52:28 -0600

    Initial commit

---
+++ stories/sample.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { initializeIcons } from "@uifabric/icons";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+initializeIcons();
+
+storiesOf("Sample", module).add("Initial", () => {});
